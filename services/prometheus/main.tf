9d866dffa6c327851c8da900d78de24ea9b6d4cc
shantanu28sharma <shantanu28sharma@gmail.com>
2022-01-31 14:14:37 +0530

    remove data

--- services/prometheus/main.tf
+++ services/prometheus/main.tf
@@ -9,13 +9,13 @@ locals {
   }
 }
 
-data "aws_subnet_ids" "private" {
-  vpc_id = var.vpc_id
+# data "aws_subnet_ids" "private" {
+#   vpc_id = var.vpc_id
 
-  tags = {
-    Tier = "Private"
-  }
-}
+#   tags = {
+#     Tier = "Private"
+#   }
+# }
 
 resource "aws_autoscaling_group" "prometheus" {
   name                      = var.name
@@ -25,14 +25,7 @@ resource "aws_autoscaling_group" "prometheus" {
   health_check_grace_period = 300
   health_check_type         = "EC2"
   launch_configuration      = aws_launch_configuration.prometheus.name
-  # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
-  # force an interpolation expression to be interpreted as a list by wrapping it
-  # in an extra set of list brackets. That form was supported for compatibility in
-  # v0.11, but is no longer supported in Terraform v0.12.
-  #
-  # If the expression in the following list itself returns a list, remove the
-  # brackets to avoid interpretation as a list of lists. If the expression
-  # returns a single list item then leave it as-is and remove this TODO comment.
+  
   vpc_zone_identifier  = var.vpc_subnets
   termination_policies = ["OldestInstance"]
   availability_zones   = [var.availability_zone]
